5e8476e9e882010a7b1e1466e16680b0c6609218
K Belardinelli <k@kangabell.co>
2017-05-13 17:31:36 -0400

    info/about page

--- info.php
+++ info.php
@@ -1,24 +1,26 @@
 <?php include ('partials/header.php'); ?>
 
-<section class="details">
-
-	<h2>Contact</h2>
-	<p>info at mar pvd dot com</p>
-	<p>P.O. Box 23285, Providence RI 02903</p>
-
-</section>
-
-<section class="details">
+<section>
 
 	<h2>About</h2>
-	<blockquote>
-		Mar released one of my all time favorite demos in 2015. <em>Trust In Nothing</em> is their first full length of soul crushing doom. The two piece focus on slooooooooow and raw doom that has a nice punk feel to it. The lyrics are emotional and deal with heavy issues ("These lines aren't cuts, they're my map"). Honestly, my words can't do justice at how good Mar is. I completely adore this band and their music. If you like doom that is slow and heavy, pick this up.
-	</blockquote>
-	<p>- Nathan Sizemore @ <a href="https://xtapolapometal.wordpress.com/2016/09/19/review-round-up/">xtapolapometal</a></p>
+	<p>
+		loud heavy noise as a vehicle for psychic survival.
+	</p>
+	<p>
+		<em>(a doom/punk/sludge two-piece with lyrics about emotional trauma and recovery.)</em>
+	</p>
 
 	<h3>Members</h3>
 	<p><a href="http://kaybelardinelli.com">Kay Belardinelli</a></p>
 	<p><a href="http://plastic-idolatry.com/erik/">Eir&#237;kr &#197;sheim</a></p>
+
+</section>
+
+<section>
+
+	<h2>Contact</h2>
+	<p>info at mar pvd dot com</p>
+	<p>P.O. Box 23285, Providence RI 02903</p>
 
 </section>
 
