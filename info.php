8ec4c958e5a8508aeab48e521b04e89e8fa5c45e
K Belardinelli <k@kangabell.co>
2017-05-14 11:33:16 -0400

    add 'Past Shows' section

--- info.php
+++ info.php
@@ -24,4 +24,35 @@
 
 </section>
 
+<section>
+
+	<h2>Past Shows</h2>
+
+	<p>
+		<strong>March 11 2017</strong> &#151; w/ Think Tank, The Oracle @ Machines With Magnets (Pawtucket, RI)
+	</p>
+	<p>
+		<strong>March 5 2017</strong> &#151; w/ Davidians, N.E.G., Penetrode @ LAVA (Philadelphia, PA)
+	</p>
+	<p>
+		<strong>March 3 2017</strong> &#151; w/ Northern Liberties, Hulk Smash @ Sound Hole (Philadelphia, PA)
+	</p>
+	<p>
+		<strong>February 1 2017</strong> &#151; w/ Occult Blood, Flinstones, Double Dragon @ Machines With Magnets (Pawtucket, RI)
+	</p>
+	<p>
+		<strong>October 21 2016</strong> &#151; w/ Empty Grows Every Bed, Mary Burke @ Psychic Readings (Providence, RI)
+	</p>
+	<p>
+		<strong>April 16 2016</strong> &#151; w/ Mossy Fiber, Nan Dejour, Sultanique @ 52 Chapped Lips (Providence, RI)
+	</p>
+	<p>
+		<strong>July 12 2015</strong> &#151; w/ Rectrix, PWR RTLZ @ Red Room (Providence, RI)
+	</p>
+	<p>
+		<strong>May 5 2015</strong> &#151; w/ Börn, Vitka, Pvblic Bath @ Red Room (Providence, RI)
+	</p>
+
+</section>
+
 <?php include ('partials/footer.php'); ?>
