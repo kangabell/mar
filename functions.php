8da58a8a7395102fbded86dea433994fb41a97ba
K Belardinelli <k@kangabell.co>
2017-06-22 12:15:28 -0400

    create function for anchor links

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
 
+	// echo anchor link
+
+	function hyperlink($name,$slug) {
+
+		// if no slug given, default to name
+		if ( !isset($slug) ) {
+			$slug = $name;
+		}
+
+		// beginning of anchor link
+		echo '<a href="' . $slug . '.php"';
+
+		// if url is the same as slug + '.php', mark it
+		if ( (basename($_SERVER['PHP_SELF'],'.php')) == $slug ) {
+			echo ' class="current" ';
+		}
+
+		// end of anchor link
+		echo '>' . $name . '</a>';
+		
+	}
+
+
 	// iterate through files in a directory to find most recently updated
 
 	function update($directory) {
