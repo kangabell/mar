8c3ccabc8d7002465277b4e2252f1cb6275cc745
K Belardinelli <k@kangabell.co>
2017-07-04 14:04:36 -0400

    add favicons

--- partials/header.php
+++ partials/header.php
@@ -29,8 +29,11 @@ site by kay belardinelli.
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Two-piece doom metal band from Providence, Rhode Island">
+	<meta name="theme-color" content="#fafbf7">
 
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 
 </head>
 
