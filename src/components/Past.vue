b6e9bee517fb789dd34e99fc4283c64f9592b3fc
K Belardinelli <k@kangabell.co>
2017-07-12 14:42:03 -0400

    move History page data out of the markup

--- src/components/Past.vue
+++ src/components/Past.vue
@@ -6,9 +6,9 @@
 
 			<h2>Releases</h2>
 			<ul class="bulleted">
-				<li><a href="https://m-a-r.bandcamp.com">Fill Your Lungs</a> <em>2017</em></li>
-				<li><a href="https://m-a-r.bandcamp.com/album/trust-in-nothing">Trust in Nothing</a> <em>2016</em></li>
-				<li><a href="https://m-a-r.bandcamp.com/album/demo">Demo</a> <em>2015</em></li>
+				<li v-for="release in releases">
+					<a v-bind:href="release.url">{{ release.name }}</a> <em>{{ release.date }}</em>
+				</li>
 			</ul>
 		</section>
 
@@ -16,47 +16,8 @@
 
 			<h2>Past Shows</h2>
 
-			<p>
-				<strong>June 28</strong> &#151; w/ Think Tank, Opening Bell, Exposed as Rot @ Osborn House (New Haven CT)
-			</p>
-			<p>
-				<strong>June 29</strong> &#151; w/ Think Tank, Big Bliss, Dog Shepherd @ Trans-Pecos (Queens NY)
-			</p>
-			<p>
-				<strong>June 30</strong> &#151; w/ Think Tank, Donor, Blind Raftery @ Overlook Hotel (Philadelphia PA)
-			</p>
-			<p>
-				<strong>July 1 2017</strong> &#151; w/ Thou, Cloudrat, Moloch @ Aurora (Providence RI)
-			</p>
-			<p>
-				<strong>July 1 2017</strong> &#151; w/ Think Tank, Perennial, Milky Wimpshake, Electric Mail Myself To Thoreau @ Gold Spring Hollow (Belchertown MA)
-			</p>
-			<p>
-				<strong>May 26 2017</strong> &#151; w/ visibilities, Suicide Magnets, Caroline Park @ Psychic Readings (Providence, RI)
-			</p>
-			<p>
-				<strong>March 11 2017</strong> &#151; w/ Think Tank, The Oracle @ Machines With Magnets (Pawtucket, RI)
-			</p>
-			<p>
-				<strong>March 5 2017</strong> &#151; w/ Davidians, N.E.G., Penetrode @ LAVA (Philadelphia, PA)
-			</p>
-			<p>
-				<strong>March 3 2017</strong> &#151; w/ Northern Liberties, Hulk Smash @ Sound Hole (Philadelphia, PA)
-			</p>
-			<p>
-				<strong>February 1 2017</strong> &#151; w/ Occult Blood, Flinstones, Double Dragon @ Machines With Magnets (Pawtucket, RI)
-			</p>
-			<p>
-				<strong>October 21 2016</strong> &#151; w/ Empty Grows Every Bed, Mary Burke @ Psychic Readings (Providence, RI)
-			</p>
-			<p>
-				<strong>April 16 2016</strong> &#151; w/ Mossy Fiber, Nan Dejour, Sultanique @ 52 Chapped Lips (Providence, RI)
-			</p>
-			<p>
-				<strong>July 12 2015</strong> &#151; w/ Rectrix, PWR RTLZ @ Red Room (Providence, RI)
-			</p>
-			<p>
-				<strong>May 5 2015</strong> &#151; w/ Börn, Vitka, Pvblic Bath @ Red Room (Providence, RI)
+			<p v-for="show in shows">
+				<strong>{{ show.date }}</strong> &#151; w/ {{ show.bands }} @ {{ show.location }}
 			</p>
 
 		</section>
@@ -64,3 +25,105 @@
 	</main>
 
 </template>
+
+<script>
+
+	export default {
+
+		data() {
+			return {
+				releases: [
+					{
+						name: 'Fill Your Lungs',
+						url: 'https://m-a-r.bandcamp.com/album/fill-your-lungs',
+						date: '2017'
+					},
+					{
+						name: 'Trust in Nothing',
+						url: 'https://m-a-r.bandcamp.com/album/trust-in-nothing',
+						date: '2016'
+					},
+					{
+						name: 'Demo',
+						url: 'https://m-a-r.bandcamp.com/album/demo',
+						date: '2015'
+					}
+				],
+				shows: [
+					{
+						date: 'June 28 2017',
+						bands: 'Think Tank, Opening Bell, Exposed as Rot',
+						location: 'Osborn House (New Haven CT)'
+					},
+					{
+						date: 'June 29 2017',
+						bands: 'Think Tank, Big Bliss, Dog Shepherd',
+						location: 'Trans-Pecos (Queens NY)'
+					},
+					{
+						date: 'June 30 2017',
+						bands: 'Think Tank, Donor, Blind Raftery',
+						location: 'Overlook Hotel (Philadelphia PA)'
+					},
+					{
+						date: 'July 1 2017',
+						bands: 'Thou, Cloudrat, Moloch',
+						location: 'Aurora (Providence RI)'
+					},
+					{
+						date: 'July 1 2017',
+						bands: 'Think Tank, Perennial, Milky Wimpshake, Electric Mail Myself To Thoreau',
+						location: 'Gold Spring Hollow (Belchertown MA)'
+					},
+					{
+						date: 'May 26 2017',
+						bands: 'visibilities, Suicide Magnets, Caroline Park',
+						location: 'Psychic Readings (Providence, RI)'
+					},
+					{
+						date: 'March 11 2017',
+						bands: 'Think Tank, The Oracle',
+						location: 'Machines With Magnets (Pawtucket, RI)'
+					},
+					{
+						date: 'March 5 2017',
+						bands: 'Davidians, N.E.G., Penetrode',
+						location: 'LAVA (Philadelphia, PA)'
+					},
+					{
+						date: 'March 3 2017',
+						bands: 'Northern Liberties, Hulk Smash',
+						location: 'Sound Hole (Philadelphia, PA)'
+					},
+					{
+						date: 'February 1 2017',
+						bands: 'Occult Blood, Flinstones, Double Dragon',
+						location: 'Machines With Magnets (Pawtucket, RI)'
+					},
+					{
+						date: 'October 21 2016',
+						bands: 'Empty Grows Every Bed, Mary Burke',
+						location: 'Psychic Readings (Providence, RI)'
+					},
+					{
+						date: 'April 16 2016',
+						bands: 'Mossy Fiber, Nan Dejour, Sultanique',
+						location: '52 Chapped Lips (Providence, RI)'
+					},
+					{
+						date: 'July 12 2015',
+						bands: 'Rectrix, PWR RTLZ',
+						location: 'Red Room (Providence, RI)'
+					},
+					{
+						date: 'May 5 2015',
+						bands: 'Börn, Vitka, Pvblic Bath',
+						location: 'Red Room (Providence, RI)'
+					}
+				],
+
+			}
+		},
+	}
+
+</script>
