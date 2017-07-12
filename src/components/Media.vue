53a8ab194274cf1e6bb0c9eab488c959f63f2673
K Belardinelli <k@kangabell.co>
2017-07-12 15:52:05 -0400

    move media content into data

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -2,15 +2,59 @@
 
 	<main>
 
-		<section>
-			<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=3170236442/size=large/bgcol=edeae6/linkcol=76939e/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://m-a-r.bandcamp.com/album/fill-your-lungs">Fill Your Lungs by Mar</a></iframe>	
-			<figure><a href="img/mar-16.jpg">
-				<img src="img/mar-16.jpg" alt="Mar, 2017" />
-				<figcaption>[click to enlarge]</figcaption>
-			</a></figure>
-			<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=2917446271/size=large/bgcol=edeae6/linkcol=76939e/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://m-a-r.bandcamp.com/album/trust-in-nothing">Trust in Nothing by Mar</a></iframe>
+		<section v-for="item in items">
+
+			<iframe v-if="item.type === 'iframe'" v-bind:src="item.src" seamless><a v-bind:href="item.url">{{ item.title }}</a></iframe>
+			
+			<figure v-if="item.type === 'image'">
+				<a v-bind:href="item.src">
+					<img v-bind:src="item.src" v-bind:alt="item.title" />
+					<figcaption>[click to enlarge]</figcaption>
+				</a>
+			</figure>
+
 		</section>
 
 	</main>
 
 </template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+				items: [
+					{
+						title: 'Fill Your Lungs by Mar',
+						type: 'iframe',
+						url: 'http://m-a-r.bandcamp.com/album/fill-your-lungs',
+						src: 'https://bandcamp.com/EmbeddedPlayer/album=3170236442/size=large/bgcol=edeae6/linkcol=76939e/tracklist=false/artwork=small/transparent=true/'
+					},
+					{
+						title: 'Mar, 2017',
+						type:  'image',
+						src: 'img/mar-16.jpg',
+					},
+					{
+						title: 'Trust in Nothing by Mar',
+						type: 'iframe',
+						url: 'http://m-a-r.bandcamp.com/album/trust-in-nothing',
+						src: 'https://bandcamp.com/EmbeddedPlayer/album=2917446271/size=large/bgcol=edeae6/linkcol=76939e/tracklist=false/artwork=small/transparent=true/'
+					}
+				]
+			}
+		}
+	}
+
+</script>
+
+<style>
+
+	iframe {
+		border: 0;
+		width: 100%;
+		height: 120px;
+	}
+
+</style>
