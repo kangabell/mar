83095c348c417615aed391072a4579e0652046c1
K Belardinelli <k@kangabell.co>
2017-07-31 14:06:28 -0400

    content update

--- index.php
+++ index.php
@@ -3,29 +3,32 @@
 
 <section class="announcement">
 	<div class="wrapper">
-		<p>JUNE 28 - JULY 1</p>
-		<h2>Mini Tour</h2>
-		<p>
-			Thank you to our friends in <a href="https://thinktanknoise.bandcamp.com/">Think Tank</a> for an amazing tour together!!
+		<video controls style="width:100%; max-width:500px">
+		  <source src="img/loveconquersall.mp4" type="video/mp4">
+		</video>
+		<p style="margin-top:0">
+			<a href="https://www.instagram.com/p/BWwkNarD4BX/">video by Front Row Poster</a>
 		</p>
 	</div>
+	<!-- 
 	<div class="wrapper">
 		<p>NEW TAPE</p>
 		<h2><em>Fill Your Lungs</em></h2>
 		<p>Named one of Noisey's <a href="https://noisey.vice.com/en_us/article/bjx8wd/the-30-best-overlooked-albums-of-2017-so-far">best overlooked albums of 2017</a></p>
 		<p><a href="http://m-a-r.bandcamp.com">m-a-r.bandcamp.com</a></p>
-	</div>
+	</div> -->
 </section>
 
 <section class="shows">
 	<h2>Upcoming Shows</h2>
 	<p>
 		<strong>July 19 2017</strong><br/>
-		<a href="https://xylitolband.bandcamp.com/">Xylitol</a>, <a href="https://daudyflin.bandcamp.com/">Dau&#240;yflin</a>, <a href="https://itspatsy.bandcamp.com/album/demo">Patsy</a>, Sap<br/>
-		Psychic Readings, Providence RI
+		<strike><a href="https://xylitolband.bandcamp.com/">Xylitol</a>, <a href="https://daudyflin.bandcamp.com/">Dau&#240;yflin</a>, <a href="https://itspatsy.bandcamp.com/album/demo">Patsy</a>, Sap<br/>
+		Psychic Readings, Providence RI</strike>
 	</p>
 	<p>
 		<strong>August 4 2017</strong><br/>
+		<a href="http://sunrot.bandcamp.com/">Sunrot</a>, <a href="http://godroot.bandcamp.com/">Godroot</a>, Black Phillip<br/>
 		Funky Jungle, Providence RI
 	</p>
 	<p>
