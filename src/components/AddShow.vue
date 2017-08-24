7607d253b65c16919f4edaabe95330a71f44ef33
K Belardinelli <k@kangabell.co>
2017-08-24 17:02:33 -0400

    add validation with Vee-Validate plugin

--- src/components/AddShow.vue
+++ src/components/AddShow.vue
@@ -6,16 +6,18 @@
 
 			<h2>Add New Show</h2>
 
-			<form>
+			<form @submit.prevent="submit">
 
 				<div>
 					<label for="date">Date *</label>
-					<input v-model="show.date" type="date" id="date" />
+					<input v-model="show.date" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('date') }" data-vv-delay="1000" placeholder="DD/MM/YYYY" type="text" name="date" />
+					<span v-show="errors.has('date')" class="is-danger">{{ errors.first('date') }}</span>
 				</div>
 
 				<div>
 					<label for="location">Location *</label>
-					<input v-model="show.location" type="text" />
+					<input v-model="show.location" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('location') }" data-vv-delay="1000" type="text" name="location" />
+					<span v-show="errors.has('location')" class="help is-danger">{{ errors.first('location') }}</span>
 				</div>
 
 				<div class="band" v-for="band in show.bands">
@@ -30,7 +32,9 @@
 					<a class="button add" @click="add">+</a>
 				</div>
 
-				<a class="button" v-on:click="addShow">Add Show</a>
+				<div>* <em>required</em></div>
+
+				<button class="button" type="submit">Add Show</button>
 
 			</form>
 
@@ -62,25 +66,27 @@
 			    count: 0,
 			    submitted: false
 			}
-		  },
-		  firebase: {
-		    shows: db.ref('shows')
-		  },
-		  methods: {
-		    add: function() {
-		      this.show.bands.push({
-		        count: ++this.count
-		      });
-		    },
-		    addShow: function(){
-		    	this.$firebaseRefs.shows.push({
-		    		date: this.show.date,
-		    		location: this.show.location,
-		    		bands: this.show.bands
-		    		
-		    	}).then( this.submitted = true )
-		    }
-		  }
+		},
+		firebase: {
+			shows: db.ref('shows')
+		},
+		methods: {
+			add: function() {
+			  this.show.bands.push({
+			    count: ++this.count
+			  });
+			},
+			submit: function(){
+				this.$validator.validateAll()
+				.then( (result) => { if (result) {
+				    this.$firebaseRefs.shows.push({
+				    	date: this.show.date,
+				    	location: this.show.location,
+				    	bands: this.show.bands
+				    }).then( this.submitted = true ) 
+				} } )
+			}
+		}
 	}
 
 </script>
@@ -91,9 +97,12 @@
 		text-align: left;
 	}
 
+	form > div {
+		margin-top: 1rem;
+	}
+
 	label {
 		display: block;
-		margin-top: 1rem;
 	}
 
 	.band {
@@ -135,6 +144,16 @@
 		display: block;
 		float: right;
 		margin-top: 2rem;
+	}
+
+	input.is-danger {
+		border: 1px solid red;
+	}
+
+	span.is-danger {
+		color: red;
+		display: block;
+		font-size: 0.85em;
 	}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import VueFire from 'vuefire'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes'
+import VeeValidate from 'vee-validate'
 
 Vue.use(VueFire)
 Vue.use(VueRouter);
+Vue.use(VeeValidate);
 
 const router = new VueRouter({
 	routes: Routes,
