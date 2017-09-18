06a732735bd421386f5dc544d7b7df566e0f000c
K Belardinelli <k@kangabell.co>
2017-09-18 15:25:37 -0400

    basic layout and style for header and homepage

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,14 +2,14 @@
 
 	<header>
 
-		<h1><a href="./"><img src="assets/img/logo.png" alt="Mar" class="logo" /></a></h1>
-
 		<nav>
 			<router-link to="/" exact>News</router-link>
 			<router-link to="/info" exact>Info</router-link>
 			<router-link to="/media" exact>Media</router-link>
 			<router-link to="/history" exact>History</router-link>
 		</nav>
+
+		<h1><a href="./"><img src="assets/img/logo.png" alt="Mar" class="logo" /></a></h1>
 
 	</header>
 
