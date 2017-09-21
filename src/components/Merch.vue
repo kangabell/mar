b99786112ef9526a20402c2ed136ac946957eabf
K Belardinelli <k@kangabell.co>
2017-09-21 15:21:00 -0400

    add the rest of the Merch data

--- src/components/Merch.vue
+++ src/components/Merch.vue
@@ -28,25 +28,43 @@
 						title: 'Demo Cassette',
 						img: 'https://f4.bcbits.com/img/0009962451_37.jpg',
 						url: 'https://m-a-r.bandcamp.com/album/demo',
-						price: '2'
+						price: 2
 					},
 					{
 						title: 'Fill Your Lungs Cassette',
 						img: 'https://f4.bcbits.com/img/0010427896_37.jpg',
 						url: 'https://m-a-r.bandcamp.com/album/fill-your-lungs',
-						price: '2'
+						price: 2
 					},
 					{
 						title: 'Patch',
 						img: 'https://f4.bcbits.com/img/0010527769_37.jpg',
 						url: 'https://m-a-r.bandcamp.com/merch/patch',
-						price: '2'
+						price: 2
 					},
 					{
 						title: '"Murmurs..." VolI Zine',
 						img: 'https://f4.bcbits.com/img/0008143325_37.jpg',
 						url: 'https://m-a-r.bandcamp.com/merch/murmurs-chants-and-screams-zine',
-						price: '2'
+						price: 2
+					},
+					{
+						title: 'Trust in Nothing Cassette',
+						img: 'https://f4.bcbits.com/img/0008143194_37.jpg',
+						url: 'https://m-a-r.bandcamp.com/album/trust-in-nothing',
+						price: 5
+					},
+					{
+						title: 'Cassette and Zine',
+						img: 'https://f4.bcbits.com/img/0008145430_37.jpg',
+						url: 'https://m-a-r.bandcamp.com/merch/cassette-tape-zine',
+						price: 6
+					},
+					{
+						title: 'T-Shirt',
+						img: 'https://f4.bcbits.com/img/0010698788_37.jpg',
+						url: 'https://m-a-r.bandcamp.com/merch/t-shirt',
+						price: 10
 					}
 
 				]
