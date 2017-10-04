945f76b6cbd0040166507e903a0cfb6b8bbe2e61
K Belardinelli <k@kangabell.co>
2017-10-04 17:41:22 -0400

    add schema.org itemprop stuff for contact info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,15 +2,15 @@
 
 	<main>
 
-		<section class="contact">
+		<section class="contact" itemscope itemtype="http://schema.org/MusicGroup">
 
 			<h2>Contact</h2>
-			<p>{{ contact[2].value }}</p>
-			<p>
-				<span class="address">{{ contact[0].value }}</span><br/>
-				<span class="city">{{ contact[1].value }}</span>
-				<span class="state">{{ contact[3].value }}</span>
-				<span class="zip">{{ contact[4].value }}</span>
+			<p itemprop="email">{{ contact[2].value }}</p>
+			<p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+				<span class="address" itemprop="streetAddress">{{ contact[0].value }}</span><br/>
+				<span class="city" itemprop="addressLocality">{{ contact[1].value }}</span>
+				<span class="state" itemprop="addressRegion">{{ contact[3].value }}</span>
+				<span class="zip" itemprop="postalCode">{{ contact[4].value }}</span>
 			</p>
 			<p><a class="bandcamp img" href="https://m-a-r.bandcamp.com/"><span class="icon"></span> <span class="text">bandcamp</span></a></p>
 			<p class="small">this site is <a href="https://github.com/kangabell/mar">open source</a>.</p>
