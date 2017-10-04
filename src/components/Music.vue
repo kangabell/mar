6b4b8942af17015be80e3035ebbe0a44ebd1f474
K Belardinelli <k@kangabell.co>
2017-10-04 13:53:29 -0400

    incorporate bandcamp embed styling into code. concatenate w/album ID stored in database.

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -4,7 +4,7 @@
 
 		<section v-for="item in items">
 
-			<iframe v-if="item.type === 'iframe'" v-bind:src="item.src" seamless><a v-bind:href="item.url">{{ item.title }}</a></iframe>
+			<iframe v-if="item.type === 'iframe'" v-bind:src=" 'https://bandcamp.com/EmbeddedPlayer/album=' + item.albumID + '/size=large/bgcol=edeae6/linkcol=76939e/tracklist=true' " seamless><a v-bind:href="item.url">{{ item.title }}</a></iframe>
 
 		</section>
 
