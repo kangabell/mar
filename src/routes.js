e41a3d8c007b11917d3edb0965097efb0cd94bae
K Belardinelli <k@kangabell.co>
2017-10-10 15:53:45 -0400

    create 404 page

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import shows from './components/shows.vue';
 import music from './components/music.vue';
 import merch from './components/merch.vue';
 import info from './components/info.vue';
+import p404 from './components/p404.vue';
 
 export default [
 	{ path: '/', component: home},
 	{ path: '/shows', component: shows},
 	{ path: '/music', component: music },
 	{ path: '/merch', component: merch },
-	{ path: '/info', component: info }
+	{ path: '/info', component: info },
+	{ path: '*', component: p404 }
 ]
