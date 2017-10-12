c95f046898f56f40b094e87685c2c915843b98b2
K Belardinelli <k@kangabell.co>
2017-10-12 14:45:36 -0400

    move homepage content into firebase db

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,12 +6,32 @@
 
 		<img src="assets/img/logo.png" alt="Mar" />
 
-		<blockquote cite="https://noisey.vice.com/en_us/article/bjx8wd/the-30-best-overlooked-albums-of-2017-so-far">
-			<p>"The Providence, Rhode Island duo hurtle themselves into waves of apocalyptic sludge and dire noise with zero regard for their own safety; the riffs come fitfully, lurching and pulling like a rabid dog straining at his leash, as vocalist Kay Belardinelli exorcises her demons in real time, gasping for air and calling down thunder. The lyrics tackle the pain and power of recovery and trauma, imbuing the album's aggrieved, menacing character with a deeply personal bent that only deepens its sense of overwhelming darkness."</p>
-			<cite>- Kim Kelly, <a href="https://noisey.vice.com/en_us/article/bjx8wd/the-30-best-overlooked-albums-of-2017-so-far">Noisey</a></cite>
+		<blockquote v-bind:cite="quote.url">
+			<p>"{{ quote.content }}"</p>
+			<cite>- {{ quote.author }}, <a v-bind:href="quote.url">Noisey</a></cite>
 		</blockquote>
 
 	</main>
 
 </transition>
 </template>
+
+<script>
+
+	import {db} from '../firebase';
+
+	export default {
+		data() {
+			return {
+				quote: {}
+			}
+		},
+		firebase: {
+			quote: {
+				source: db.ref('quote'),
+				asObject: true
+			}
+		}
+}
+
+</script>
