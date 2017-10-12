bbc8b74572435644218cd9c753e224f83320c9f7
K Belardinelli <k@kangabell.co>
2017-10-12 15:56:05 -0400

    move bandcamp URL into db object with contact info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,17 +3,17 @@
 
 	<main>
 
-		<section class="contact" itemscope itemtype="http://schema.org/MusicGroup">
+		<section class="info" itemscope itemtype="http://schema.org/MusicGroup">
 
 			<h2>Contact</h2>
-			<p itemprop="email">{{ contact.email }}</p>
+			<p itemprop="email">{{ info.email }}</p>
 			<p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-				<span class="address" itemprop="streetAddress">{{ contact.address }}</span><br/>
-				<span class="city" itemprop="addressLocality">{{ contact.city }}</span>
-				<span class="state" itemprop="addressRegion">{{ contact.state }}</span>
-				<span class="zip" itemprop="postalCode">{{ contact.zip }}</span>
+				<span class="address" itemprop="streetAddress">{{ info.address }}</span><br/>
+				<span class="city" itemprop="addressLocality">{{ info.city }}</span>
+				<span class="state" itemprop="addressRegion">{{ info.state }}</span>
+				<span class="zip" itemprop="postalCode">{{ info.zip }}</span>
 			</p>
-			<p><a class="bandcamp img" href="https://m-a-r.bandcamp.com/"><span class="icon"></span> <span class="text">bandcamp</span></a></p>
+			<p><a class="bandcamp img" v-bind:href="info.bandcampURL"><span class="icon"></span> <span class="text">bandcamp</span></a></p>
 			<p class="small">this site is <a href="https://github.com/kangabell/mar">open source</a>.</p>
 
 		</section>
@@ -34,7 +34,7 @@
 				articles: {},
 				members: {},
 				vendors: {},
-				contact: {}
+				info: {}
 			}
 		},
 		firebase: {
@@ -50,8 +50,8 @@
 			vendors: {
 				source: db.ref('vendors'),
 			},
-			contact: {
-				source: db.ref('contact'),
+			info: {
+				source: db.ref('info'),
 				asObject: true
 			}
 		}
--- src/components/Merch.vue
+++ src/components/Merch.vue
@@ -7,11 +7,11 @@
 
 			<div v-for="product in products" class="product" v-bind:class="{ featured: product.featured }">
 
-				<a v-bind:href="product.url" class="img"><img v-bind:src="product.img" v-bind:alt="product.title" /></a>
+				<a v-bind:href="info.bandcampURL + product.path" class="img"><img v-bind:src="product.img" v-bind:alt="product.title" /></a>
 				<div class="text">
 					<p class="title">{{ product.title }}</p>
 					<p class="description" v-if="product.description">{{ product.description }}</p>
-					<p class="price"><a v-bind:href="product.url">&#36;{{ product.price }}</a></p>
+					<p class="price"><a v-bind:href="info.bandcampURL + product.path">&#36;{{ product.price }}</a></p>
 				</div>
 					
 			</div>
@@ -19,7 +19,7 @@
 		</section>
 
 		<div class="more">
-			<a class="button" v-bind:href="merchURL">More Merch</a>
+			<a class="button" v-bind:href="info.bandcampURL + '/merch'">More Merch</a>
 		</div>
 
 	</main>
@@ -34,13 +34,17 @@
 	export default {
 		data() {
 			return {
-				merchURL: 'https://m-a-r.bandcamp.com/merch/',
-				products: {}
+				products: {},
+				info: {}
 			}
 		},
 		firebase: {
 			products: {
 				source: db.ref('products')
+			},
+			info: {
+				source: db.ref('info'),
+				asObject: true
 			}
 		}
 	}
