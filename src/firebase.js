6a08e46c104a28f25c0e35796462348ac81a1e6a
deconstructionalism <deconstructionalism@gmail.com>
2019-03-19 21:30:45 -0400

    Removes explicit API values from src/firebase.js.
    - API values are insecure stored publically GitHub
    - instead, created untracked .env and .env.production files to hold
      secret values
    - these values are loaded into webpack and passed to the application
      under process.env namespace
    - contents of .env files can be shared for use in development/production
      environments using a non-public method from now on, ensuring secret
      information is not being shared with the public

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,12 @@
 import Firebase from 'firebase'
 
 const firebaseApp = Firebase.initializeApp({
-
-	// PROD:
-	apiKey: "AIzaSyDglCXmo4u9TcK85k_d_xO6Bh4xPWMMbEk",
-	authDomain: "mar-website-30a21.firebaseapp.com",
-	databaseURL: "https://mar-website-30a21.firebaseio.com",
-	storageBucket: "mar-website-30a21.appspot.com",
-	messagingSenderId: "971613104370"
-
-	// DEV:
-	// apiKey: "AIzaSyCYin13y1r_WjpXGY7MK5wKq6BL1F8LEbw",
-	// authDomain: "mar-website-dev.firebaseapp.com",
-	// databaseURL: "https://mar-website-dev.firebaseio.com",
-	// projectId: "mar-website-dev",
-	// storageBucket: "mar-website-dev.appspot.com",
-	// messagingSenderId: "128897541679"
-
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	storageBucket: process.env.STORAGE_BUCKET,
+	projectId: process.env.PROJECT_ID,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID
 });
 
 export const db = firebaseApp.database();
