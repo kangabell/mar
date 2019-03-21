beee523f1d83dfb473997ab2657087fd073d198d
kangabell <k@kangabell.co>
2019-03-20 20:25:12 -0400

    remove the 'PROJECT_ID' value from firebase as it does not seem to be necessary

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,6 @@ const firebaseApp = Firebase.initializeApp({
 	authDomain: process.env.AUTH_DOMAIN,
 	databaseURL: process.env.DATABASE_URL,
 	storageBucket: process.env.STORAGE_BUCKET,
-	projectId: process.env.PROJECT_ID,
 	messagingSenderId: process.env.MESSAGING_SENDER_ID
 });
 
