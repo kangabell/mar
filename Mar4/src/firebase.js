27b233b46cb51ee2a288023ce16bb483a34c96f0
kangabell <k@kangabell.co>
2022-06-12 13:40:15 -0400

    merge some old stuff with new

--- Mar4/src/firebase.js
+++ Mar4/src/firebase.js
@@ -1,4 +1,4 @@
-import Firebase from 'firebase'
+import Firebase from '@firebase/app'
 
 const firebaseApp = Firebase.initializeApp({
 	apiKey: process.env.API_KEY,
--- Mar4/src/main.js
+++ Mar4/src/main.js
@@ -7,3 +7,32 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+
+
+// import './firebase';
+
+// import Vue from 'vue'
+// import VueFire from 'vuefire'
+// import App from './App.vue'
+// import VueRouter from 'vue-router'
+// import Routes from './routes'
+
+// Vue.use(VueFire)
+// Vue.use(VueRouter);
+
+// const router = new VueRouter({
+// 	routes: Routes,
+// 	mode: 'history',
+// 	scrollBehavior (to, from, savedPosition) {
+// 		return { x: 0, y: 0 }
+// 	}
+// });
+
+// export const bus = new Vue();
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+//   router: router
+// })
